b2bfbc0071f495293efe726e5c205513cc05bc06
Kikuchi Shunsuke <kyunto.site@gmail.com>
2022-10-07 16:36:02 +0900

    アイコンを追加しました

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,7 +1,17 @@
 import React from "react";
+import TwitterIcon from "@mui/icons-material/Twitter";
 
 const Sidebar = () => {
-    return <div></div>;
+    return (
+        <div className="sidebar">
+            {/* アイコン */}
+            <TwitterIcon className="sidebar_twitterIcon" />
+
+            {/* SideBarOption */}
+
+            {/* 投稿ボタン */}
+        </div>
+    );
 };
 
 export default Sidebar;
