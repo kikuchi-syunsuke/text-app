9b0a57e2a63bb6d1aca8c23ea10a49ac6168b741
Kikuchi Shunsuke <kyunto.site@gmail.com>
2022-10-07 16:56:12 +0900

    各アイコンとテキストを表示しました

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,7 +1,13 @@
 import React from "react";
 import TwitterIcon from "@mui/icons-material/Twitter";
 import HomeIcon from "@mui/icons-material/Home";
-
+import SearchIcon from "@mui/icons-material/Search";
+import MailOutlineIcon from "@mui/icons-material/MailOutline";
+import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
+import BookmarkBorderIcon from "@mui/icons-material/BookmarkBorder";
+import ListAltIcon from "@mui/icons-material/ListAlt";
+import PermIdentityIcon from "@mui/icons-material/PermIdentity";
+import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
 import SidebarOption from "./SidebarOption";
 
 const Sidebar = () => {
@@ -12,12 +18,14 @@ const Sidebar = () => {
 
             {/* SideBarOption */}
             <SidebarOption text="ホーム" Icon={HomeIcon} />
-            <SidebarOption text="ホーム" Icon={HomeIcon} />
-            <SidebarOption text="ホーム" Icon={HomeIcon} />
-            <SidebarOption text="ホーム" Icon={HomeIcon} />
-            <SidebarOption text="ホーム" Icon={HomeIcon} />
-            <SidebarOption text="ホーム" Icon={HomeIcon} />
-            <SidebarOption text="ホーム" Icon={HomeIcon} />
+            <SidebarOption text="話題を検索" Icon={SearchIcon} />
+            <SidebarOption text="通知" Icon={NotificationsNoneIcon} />
+            <SidebarOption text="メッセージ" Icon={MailOutlineIcon} />
+            <SidebarOption text="ブックマーク" Icon={BookmarkBorderIcon} />
+            <SidebarOption text="リスト" Icon={ListAltIcon} />
+            <SidebarOption text="プロフィール" Icon={PermIdentityIcon} />
+            <SidebarOption text="もっと見る" Icon={MoreHorizIcon} />
+
             {/* 投稿ボタン */}
         </div>
     );
