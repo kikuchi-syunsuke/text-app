ca64fb97c5f124ed1dfecd02c3f33c844a275f9c
Kikuchi Shunsuke <kyunto.site@gmail.com>
2022-10-11 15:02:40 +0900

    Timelineコンポーネントを追加しました

---
+++ src/components/timeline/Timeline.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Timeline = () => {
+    return (
+        <div className="timeline">
+            {/* Header */}
+            <div clessName="timelineHeader">
+                <h2>ホーム</h2>
+            </div>
+            {/* TweetBox */}
+
+            {/* Post */}
+        </div>
+    );
+};
+
+export default Timeline;
