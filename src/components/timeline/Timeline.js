16320af0d1160ad8b615c2bcea2d9f0eb4d60e06
kikuchi-syunsuke <kyunto.site@gmail.com>
2022-10-12 15:59:09 +0900

    Postコンポーネントへprops渡しました

--- src/components/timeline/Timeline.js
+++ src/components/timeline/Timeline.js
@@ -13,10 +13,14 @@ const Timeline = () => {
             {/* TweetBox */}
             <TweetBox />
             {/* Post */}
-            <Post />
-            <Post />
-            <Post />
-            <Post />
+            <Post
+                displayName="プログラミングテスト"
+                username="zrts_kkch"
+                verified={true}
+                text="初めてのツイート"
+                avatar="http://shincode.info/wp-content/uploads/2021/12/icon.png"
+                image="https://source.unsplash.com/random"
+            />
         </div>
     );
 };
