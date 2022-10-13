819f260d71572cdedcf48fade612565e71ad61d8
kikuchi-syunsuke <kyunto.site@gmail.com>
2022-10-13 16:10:07 +0900

    投稿するとリアルタイムで表示するようにしました

--- src/components/timeline/Timeline.js
+++ src/components/timeline/Timeline.js
@@ -3,16 +3,22 @@ import Post from "./Post";
 import "./Timeline.css";
 import TweetBox from "./TweetBox";
 import db from "../../firebase";
-import { collection, getDocs, orderBy, query } from "firebase/firestore";
+import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
+import FlipMove from "react-flip-move";
 
 const Timeline = () => {
     const [posts, setPosts] = useState([]);
     useEffect(() => {
         const postData = collection(db, "posts");
         const q = query(postData, orderBy("timestamp", "desc"));
-        getDocs(q).then((querySnapshot) => {
+        //getDocs(q).then((querySnapshot) => {
+        //    setPosts(querySnapshot.docs.map((doc) => doc.data()))
+        //});
+
+        onSnapshot(q, (querySnapshot) => {
             setPosts(querySnapshot.docs.map((doc) => doc.data()))
-        });
+        })
+
     }, [])
 
 
@@ -25,17 +31,19 @@ const Timeline = () => {
             {/* TweetBox */}
             <TweetBox />
             {/* Post */}
-            {posts.map((post) => (
-                <Post
-                key={post.text}
-                    displayName={post.displayName}
-                    username={post.username}
-                    verified={post.verified}
-                    text={post.text}
-                    avatar={post.avatar}
-                    image={post.image}
-                />
-            ))}
+            <FlipMove>
+                {posts.map((post) => (
+                    <Post
+                    key={post.text}
+                        displayName={post.displayName}
+                        username={post.username}
+                        verified={post.verified}
+                        text={post.text}
+                        avatar={post.avatar}
+                        image={post.image}
+                    />
+                ))}
+            </FlipMove>
         </div>
     );
 };
