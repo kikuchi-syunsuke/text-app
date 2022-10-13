5bf8a8788f85e98679d54a5303533000c16a24fa
kikuchi-syunsuke <kyunto.site@gmail.com>
2022-10-13 10:03:51 +0900

    firebaseの初期化を行いました

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBeilWhW4WYmdx4qM6i6woOEx7WYrjPsuU",
+    authDomain: "text-app-ecc5e.firebaseapp.com",
+    projectId: "text-app-ecc5e",
+    storageBucket: "text-app-ecc5e.appspot.com",
+    messagingSenderId: "330031838648",
+    appId: "1:330031838648:web:f83a5e3f00479ae46e0b4d"
+  };
+
+  const app = initializeApp(firebaseConfig);
